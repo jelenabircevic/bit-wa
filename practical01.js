1ce31ce19ffdec7cc1687821dd1b67f04ab3a212
BIT Student <classroom@bgit.rs>
2018-03-13 16:58:35 +0100

    fetch and promises

--- practical01.js
+++ practical01.js
@@ -3,21 +3,31 @@ console.log(arr100); */
 
 let arr100 = Array(100).fill().map((v, i) => i).map((elem, i) => new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(i);
-    }, parseInt(Math.random() * 500));
+        const marker = Math.random();
+        if (marker < 0.85) {
+            resolve(i);
+        } else {
+            reject(`reject ${i}`);
+        }
+    }, parseInt(500));
 }))
 
 arr100.forEach((elem) => {
     elem.then((value) => {
-        console.log(`done...${value}`);
+        console.log(value)
+    }, (value) => {
+        console.log(value)
     })
 })
 
-let first = Promise.race(arr100);
-first.then((value) => {
-    console.log(`Completed first...${value}`);
-}, first);
+Promise.race(arr100).then((value) => {
+    console.log(`Completed first: ${value}, time: ${(new Date()).getTime()}`);
+}, (value) => {
+    console.log(`first ${value}`)
+});
 
 Promise.all(arr100).then(() => {
-    console.log('All complete!');
+    console.log(`All complete! time: ${(new Date()).getTime()}`);
+}, (value) => {
+    setTimeout(console.log, 2000, `Not all completed`);
 })
