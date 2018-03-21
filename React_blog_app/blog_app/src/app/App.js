4be0c44d7fb0f77e5d9ab5fbbbb3e2415a977e39
jelenabircevic <jelena.bircevic@gmail.com>
2018-03-21 23:57:20 +0100

    [bpf-8] done

--- React_blog_app/blog_app/src/app/App.js
+++ React_blog_app/blog_app/src/app/App.js
@@ -3,6 +3,7 @@ import Header from './partials/Header';
 import Footer from './partials/Footer';
 import users from '../services/Users';
 import UsersList from './users/UsersList';
+import Search from './users/Search';
 import './App.css'
 
 class App extends Component {
@@ -15,14 +16,16 @@ class App extends Component {
     }
     this.changeView = this.changeView.bind(this)
     this.refreshUsers = this.refreshUsers.bind(this)
+    this.filterUsers = this.filterUsers.bind(this)
   }
-
+  
   componentDidMount() {
     if (localStorage.getItem('data')) {
       this.setState({
         users: JSON.parse(localStorage.getItem('data')),
-        grid: localStorage.getItem('grid')
-    })
+        grid: localStorage.getItem('grid'),
+        viewButton: (localStorage.getItem('grid')) ? "view_list" : "view_module",
+      })
     } else {
       this.refreshUsers()
     }
@@ -42,16 +45,26 @@ class App extends Component {
     users.getData()
       .then(res => {
         this.setState({
-          users: res
+          users: res,
         })
         localStorage.setItem('data', JSON.stringify(res))
       })
+  }
+
+  filterUsers(event) {
+    const query = event.target.value;
+    this.setState((prevState, props) => {
+      return {
+        users: JSON.parse(localStorage.getItem('data')).filter((user) => user.fullName.includes(query))
+      }
+    })
   }
 
   render() {
     return (
       <div className="App">
         <Header title="Bit Persons" refreshUsers={this.refreshUsers} changeView={this.changeView} viewButton={this.state.viewButton} />
+        <Search filterUsers={this.filterUsers} />
         <UsersList grid={this.state.grid} users={this.state.users} />
         <Footer copy="2018 Copyright BITstudent" />
       </div>
---
+++ React_blog_app/blog_app/src/app/users/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Search = (props) => {
+    return (
+        <div className="row container">
+            <div className="input-field col s12">
+                <i className="material-icons prefix">search</i>
+                <input onChange={props.filterUsers} id="icon_prefix" type="text" className="validate" />
+                <label htmlFor="icon_prefix">Search Users</label>
+            </div>
+        </div>
+    )
+}
+
+export default Search
